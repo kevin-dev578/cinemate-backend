5cda4f2fb3a2c62afab4845a7aaa63775e0496a0
snipercoder96 <kmbembakiyindou@byupathway.edu>
2026-06-21 13:31:17 +0200

    Added cookies to ensure that sessions are remembered the next time someone logs in

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(session({
     secret: SESSION_SECRET, // Use the SESSION_SECRET from environment variables for security
     resave: false, // what does this do? - Don't save session if unmodified
     saveUninitialized: true, // Save uninitialized sessions (new but not modified), this means that a session will be created for every user, even if they don't log in or interact with the site, which can lead to a large number of unused sessions in the store. Consider setting this to false if you want to only create sessions for users who log in or interact with the site.
-    cookie: { maxAge: 60 * 60 * 1000 } // Session expires after 1 hour of inactivity
+    cookie: { maxAge: 60 * 60 * 1000, secure: true, sameSite: 'none' } // Session expires after 1 hour of inactivity
 }));
 
 app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies (for form submissions)
